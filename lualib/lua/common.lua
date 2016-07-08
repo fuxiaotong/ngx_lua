bb5e242b74bfd2b34f3af0b491891e447cc67717
fuxiaotong <411598110@qq.com>
2016-07-08 00:22:50 -0700

    redis and cache

--- lualib/lua/common.lua
+++ lualib/lua/common.lua
@@ -1,5 +1,8 @@
 module("lua.common", package.seeall)
 local json = require "cjson.safe"
+local redis = require "resty.redis"
+local red = redis:new()
+
 
 function json_decode( str )
     local json_value = nil
@@ -11,4 +14,99 @@ function json_encode( str )
     local json_value = nil
     pcall(function (str) json_value = json.encode(str) end, str)
     return json_value
+end
+
+--检查输入数据的合法性
+function check_args(args, require_key)
+    if not args then
+      return false
+    end
+
+    args = json_decode(args)
+    local key, value
+    for k,_ in ipairs(require_key) do
+
+        key = require_key[k]
+        value = args[key]
+
+        if nil == value then
+            return false
+        elseif #value == 0 then
+            return false
+        end
+    end
+
+    return true
+end
+
+
+function set_request_redis(args_t)
+  red:set_timeout(1000) --timeout 1 sec
+
+  local ok, err = red:connect("127.0.0.1", 6379)
+  if err then
+    return nil, err
+  end
+
+  local count, err = red:get_reused_times()
+  if 0 == count then
+    ok, err = red:auth("ubuntu")  -- 安全验证
+    if not ok then
+      return nil, err
+    end
+  elseif err then
+    return nil, err
+  end
+
+  if args_t then
+    for i,v in pairs(args_t) do
+      local ok, err = red:hmset("mydata", i, v)
+      if not ok then
+        return nil, err
+      end
+      --ngx.say("key: ",i,", value: ",v, "  set ok")
+    end
+  end
+end
+
+
+function get_data_redis()
+  local json_status = {}
+  local res, err = red:hmget("mydata","name","age")
+
+  json_status.name = res[1]
+  json_status.age = res[2]
+  local record_json = json_encode(json_status)
+
+  -- 连接池大小是100个，并且设置最大的空闲时间是 10 秒
+  local ok, err = red:set_keepalive(10000, 100)
+  if not ok then
+    return nil, err
+  end
+
+  return record_json, err
+
+end
+
+
+function get_from_cache(key)
+    local cache_ngx = ngx.shared.my_cache
+    local value = cache_ngx:get(key)
+    return value
+end
+
+function set_to_cache(args_t, exptime)
+    if not exptime then
+        exptime = 0
+    end
+    local cache_ngx = ngx.shared.my_cache
+    if args_t then
+      for key, value in pairs(args_t) do
+        local succ, err, forcible = cache_ngx:set(key, value, exptime)
+        if err then
+          return nil, err
+        end
+      end
+    end
+    return succ
 end
--- lualib/lua/lua_handle.lua
+++ lualib/lua/lua_handle.lua
@@ -1,73 +1,35 @@
 local redis = require "resty.redis"
-local commom = require "lua.common"
+local common = require "lua.common"
 
--- decode json
-local data = ngx.req.get_body_data()
+ngx.req.read_body()
 
-local str = data
-local t = commom.json_decode(str)
-if t then
-    for i,v in pairs(t) do
-      ngx.say("key: ",i,", value: ",v)
-    end
+--local args = ngx.req.get_body_data()
+local args = '{"name":"xiaohong","age":"55"}'
+--ngx.log(ngx.INFO, "data string:", data)
+if common.check_args(args, {'name','age'}) == false then
+    ngx.exit(ngx.HTTP_BAD_REQUEST)
 end
 
--- redis connect and write
+local args_t = common.json_decode(args)
 
-local red = redis:new()
-
-red:set_timeout(1000) --timeout 1 sec
-
-local ok, err = red:connect("127.0.0.1", 6379)
-if not ok then
-   ngx.say("failed to connect: ", err)
-   return
+local ok, err = common.set_request_redis(args_t)
+if err then
+  ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
 end
 
--- 请注意这里 auth 的调用过程
-local count
-count, err = red:get_reused_times()
-if 0 == count then
-   ok, err = red:auth("ubuntu")  -- 安全验证
-   if not ok then
-       ngx.say("failed to auth: ", err)
-       return
-     end
-elseif err then
-  ngx.say("failed to get reused times: ", err)
-  return
+local json_data, err = common.get_data_redis()
+if err then
+  ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
 end
 
-if t then
+ngx.say(json_data)
 
-  for i,v in pairs(t) do
+-- use cache instead of redis
 
-    local ok, err = red:hmset("mydata", i, v)
-    if not ok then
-      ngx.say("failed to set : ", err)
-      return
-    end
-    ngx.say("key: ",i,", value: ",v, "  set ok")
-
-  end
-
-end
-
-
--- read from redis
-
-local json_status = {}
-local res, err = red:hmget("mydata","name","age")
-
-json_status.name = res[1]
-json_status.age = res[2]
-local record_json = commom.json_encode(json_status)
-ngx.say("output=======>")
-ngx.say(record_json)
-
--- 连接池大小是100个，并且设置最大的空闲时间是 10 秒
-local ok, err = red:set_keepalive(10000, 100)
-if not ok then
-  ngx.say("failed to set keepalive: ", err)
-  return
-end
+-- local ok, err = common.set_to_cache(args_t, 1000)
+-- if err then
+--   ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+-- end
+--
+-- local value = common.get_from_cache("age")
+ -- ngx.say(value)
